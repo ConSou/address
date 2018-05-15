629a67bf145521c98dee06417ef909c29cef56d3
Conor Souhrada <ConorSouhrada@gmail.com>
2018-05-15 10:37:17 -0600

    checkpoint-5 commit

--- models/address_book.rb
+++ models/address_book.rb
@@ -9,17 +9,23 @@ class AddressBook
   end
 
   def add_entry(name, phone_number, email)
-
     index = 0
+
     entries.each do |entry|
-
-    if name < entry.name
-      break
+      if name < entry.name
+        break
+      end
+      index += 1
     end
-    index += 1
+    entries.insert(index, Entry.new(name, phone_number, email))
   end
 
-  entries.insert(index, Entry.new(name, phone_number, email))
-  end
+  def remove_entry(name, phone_number, email)
 
+    entries.each_with_index do |entry, index|
+      if name === entry.name
+        entries.delete_at(index)
+      end
+    end
+  end
 end
